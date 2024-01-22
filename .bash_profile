25f87e6c1c10fd9e50b951ddb9ce528387e2ddc7
Justin Spencer <justin.spencer@icf.com>
2024-01-22 11:35:17 -0500

    Adjustments to support versions and profiles

--- .bash_profile
+++ .bash_profile
@@ -8,3 +8,12 @@ generate_ssh_key(){
   fi
   ssh-keygen -t rsa -b 4096 -C $1
 }
+
+dmysql-start(){
+  docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass -e -d mysql:latest
+}
+dmysql-env(){
+  export RDS_HOSTNAME=127.0.0.1
+  export RDS_USERNAME=root
+  export RDS_PASSWORD=pass
+}
--- tf-helpers.sh
+++ tf-helpers.sh
@@ -1,5 +1,31 @@
-
+#
 # Some terraform helpers (based on convention)
+# You can either run them from the project root or from the /terraform directory
+#
+
+export ACTIVE_TF_PROFILE='unknown'
+export ACTIVE_TF=terraform@1.2.9
+# If you have several projects, this is useful to set the correct prefix
+tfprofile(){
+  export ACTIVE_TF_PROFILE=$1
+}
+
+# NOTE: Following isn't working - need to figure out why taps aren't working as desired
+# If you have several projects with varied TF versions, this can set the desired one
+# brew extract --version=1.5.7 terraform hashicorp/tap
+# brew install hashicorp/tap/terraform@1.5.7
+# terraform@1.5.7 --version
+
+# Set the active version of Terraform
+# tfversion terraform@1.5.7   # use specific version
+# tfversion terraform         # use latest
+tfversion(){
+  export ACTIVE_TF=$1
+}
+
+#
+# Initialize terraform against a particular tfstate
+#
 tfinit(){
   if [ -d "terraform" ]; then
     POP_TF="yes"
@@ -10,7 +36,12 @@ tfinit(){
     echo 'Arg $1 should be environment!'
     return
   fi
-  terraform init -backend-config=config/backend-$1.conf
+  TARGET_ENV=$(echo "$1" | awk '{split($0,env_arr,"-"); print env_arr[1]}')
+
+  echo "Using profile ${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+  export AWS_PROFILE="${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+
+  $ACTIVE_TF init -upgrade -reconfigure -backend-config=config/backend-$1.conf
 
   if [ ! -z "$POP_TF" ]; then
     POP_TF=''
@@ -18,6 +49,9 @@ tfinit(){
   fi
 }
 
+# 
+# Run the expected execution plan
+# 
 tfplan(){
   if [ -d "terraform" ]; then
     POP_TF="yes"
@@ -28,7 +62,13 @@ tfplan(){
     echo 'Arg $1 should be environment!'
     return
   fi
-  terraform plan -var-file="config/$1.tfvars"
+
+  TARGET_ENV=$(echo "$1" | awk '{split($0,env_arr,"-"); print env_arr[1]}')
+
+  echo "Using profile ${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+  export AWS_PROFILE="${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+
+  $ACTIVE_TF plan -var-file="config/$TARGET_ENV.tfvars"
 
   if [ ! -z "$POP_TF" ]; then
     POP_TF=''
@@ -36,6 +76,9 @@ tfplan(){
   fi
 }
 
+# 
+# Destroy all resources
+# 
 tfdestroy(){
   if [ -d "terraform" ]; then
     POP_TF="yes"
@@ -46,7 +89,13 @@ tfdestroy(){
     echo 'Arg $1 should be environment!'
     return
   fi
-  terraform destroy -var-file="config/$1.tfvars"
+
+  TARGET_ENV=$(echo "$1" | awk '{split($0,env_arr,"-"); print env_arr[1]}')
+
+  echo "Using profile ${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+  export AWS_PROFILE="${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+
+  $ACTIVE_TF destroy -var-file="config/$TARGET_ENV.tfvars"
 
   if [ ! -z "$POP_TF" ]; then
     POP_TF=''
@@ -54,6 +103,9 @@ tfdestroy(){
   fi
 }
 
+# 
+# Apply the terraform execution plan with before/after timestamps
+# 
 tfapply(){
   if [ -d "terraform" ]; then
     POP_TF="yes"
@@ -64,8 +116,14 @@ tfapply(){
     echo 'Arg $1 should be environment!'
     return
   fi
+
+  TARGET_ENV=$(echo "$1" | awk '{split($0,env_arr,"-"); print env_arr[1]}')
+
+  echo "Using profile ${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+  export AWS_PROFILE="${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+
   echo "Apply started at $(date)"
-  terraform apply -var-file="config/$1.tfvars"
+  $ACTIVE_TF apply -var-file="config/$TARGET_ENV.tfvars"
   echo "Apply completed at $(date)"
 
   if [ ! -z "$POP_TF" ]; then
@@ -74,6 +132,9 @@ tfapply(){
   fi
 }
 
+# 
+# Import existing resources into your tfstate
+# 
 tfimport(){
   if [ -d "terraform" ]; then
     POP_TF="yes"
@@ -92,7 +153,46 @@ tfimport(){
     echo 'Arg $3 should be the ID of the resource to import!'
     return
   fi
-  terraform import -var-file="config/$1.tfvars" $2 $3
+
+  TARGET_ENV=$(echo "$1" | awk '{split($0,env_arr,"-"); print env_arr[1]}')
+
+  echo "Using profile ${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+  export AWS_PROFILE="${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+
+  $ACTIVE_TF import -var-file="config/$TARGET_ENV.tfvars" $2 $3
+
+  if [ ! -z "$POP_TF" ]; then
+    POP_TF=''
+    popd
+  fi
+}
+
+# 
+# Migrate your current state into a new state file
+# 
+tfmigrate(){
+  if [ -d "terraform" ]; then
+    POP_TF="yes"
+    pushd ./terraform
+  fi
+
+  if [ -z "$1" ]; then
+    echo 'Arg $1 should be environment!'
+    return
+  fi
+
+  if [ $1 = "sbx" ] || [ $1 = "dev" ] || [ $1 = "qa" ] || [ $1 = "prod" ]; then
+    echo "You are not allowed to migrate into normal state files. You should migrate into a suffixed environment, e.g. 'dev-gandalf'"
+    return
+  fi
+
+  TARGET_ENV=$(echo "$1" | awk '{split($0,env_arr,"-"); print env_arr[1]}')
+
+  echo "Using profile ${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+  export AWS_PROFILE="${ACTIVE_TF_PROFILE}_$TARGET_ENV"
+
+  echo "Migrating into $1"
+  $ACTIVE_TF init -migrate-state -force-copy -backend-config=config/backend-$1.conf
 
   if [ ! -z "$POP_TF" ]; then
     POP_TF=''
